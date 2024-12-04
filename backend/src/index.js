3bc4456a111d3eda8c5506067bd880facc03507c
KevinWu <kevinwu200099@gmail.com>
2024-12-05 06:13:43 +0800

    update schema correct path

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,7 @@ const pubsub = createPubSub();
 const yoga = createYoga({
   schema: createSchema({
     typeDefs: fs.readFileSync(
-      path.resolve('./schema.graphql'),
+      path.resolve('./backend/src/schema.graphql'),
       'utf-8'
     ),
     resolvers: {
