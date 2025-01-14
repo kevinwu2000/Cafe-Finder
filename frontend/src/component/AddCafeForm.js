575ec77edba97df3a09893dc01b5875b672dbd5e
KevinWu <kevinwu200099@gmail.com>
2025-01-14 21:07:41 +0800

    Update AddCafe page

--- frontend/src/component/AddCafeForm.js
+++ frontend/src/component/AddCafeForm.js
@@ -1,80 +1,74 @@
-import * as React from 'react';
+import React from 'react';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import TextField from '@mui/material/TextField';
-import FormControlLabel from '@mui/material/FormControlLabel';
-import Checkbox from '@mui/material/Checkbox';
-import {useState} from 'react';
 
 function AddCafeForm(props) {
-
   return (
     <React.Fragment>
-        <Typography variant="h6" gutterBottom>
-            Basic Info
-        </Typography>
-        <Grid container spacing={3}>
-            <Grid item xs={12} sm={6}>
-                <TextField
-                    // id="firstName"
-                    // name="firstName"
-                    label="Café Name"
-                    fullWidth
-                    variant="standard"
-                    value={props.cafeName}
-                    onChange={(e) => {props.setCafeName(e.target.value)}}
-                />
-            </Grid>
-
-            <Grid item xs={12} sm={6}>
-                <TextField
-                    // id="lastName"
-                    // name="lastName"
-                    label="Phone Number"
-                    fullWidth
-                    variant="standard"
-                    value={props.phoneNum}
-                    onChange={(e) => {props.setPhoneNum(e.target.value)}}
-                />
-            </Grid>
-
-            <Grid item xs={12} sm={6}>
-                <TextField
-                    // id="city"
-                    // name="city"
-                    label="City"
-                    fullWidth
-                    variant="standard"
-                    value={props.city}
-                    onChange={(e) => {props.setCity(e.target.value)}}
-                />
-            </Grid>
-
-            <Grid item xs={12} sm={6}>
-                <TextField
-                    // id="state"
-                    // name="state"
-                    label="District"
-                    fullWidth
-                    variant="standard"
-                    value={props.district}
-                    onChange={(e) => {props.setDistrict(e.target.value)}}
-                />
-            </Grid>
-
-            <Grid item xs={12}>
-                <TextField
-                    // id="address1"
-                    // name="address1"
-                    label="Address"
-                    fullWidth
-                    variant="standard"
-                    value={props.address}
-                    onChange={(e) => {props.setAddress(e.target.value)}}
-                />
-            </Grid>
+      <Typography variant="h5" gutterBottom sx={{ fontWeight: 'bold', color: '#333', mb: 2 }}>
+        Basic Information
+      </Typography>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Café Name"
+            fullWidth
+            variant="outlined"
+            required
+            value={props.cafeName}
+            onChange={(e) => props.setCafeName(e.target.value)}
+            sx={{ bgcolor: '#f9f9f9', borderRadius: 1 }}
+          />
         </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="Phone Number"
+            fullWidth
+            variant="outlined"
+            value={props.phoneNum}
+            onChange={(e) => props.setPhoneNum(e.target.value)}
+            sx={{ bgcolor: '#f9f9f9', borderRadius: 1 }}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="City"
+            fullWidth
+            variant="outlined"
+            value={props.city}
+            onChange={(e) => props.setCity(e.target.value)}
+            sx={{ bgcolor: '#f9f9f9', borderRadius: 1 }}
+          />
+        </Grid>
+
+        <Grid item xs={12} sm={6}>
+          <TextField
+            label="District"
+            fullWidth
+            variant="outlined"
+            value={props.district}
+            onChange={(e) => props.setDistrict(e.target.value)}
+            sx={{ bgcolor: '#f9f9f9', borderRadius: 1 }}
+          />
+        </Grid>
+
+        <Grid item xs={12}>
+          <TextField
+            label="Address"
+            fullWidth
+            variant="outlined"
+            required
+            value={props.address}
+            onChange={(e) => props.setAddress(e.target.value)}
+            sx={{ bgcolor: '#f9f9f9', borderRadius: 1 }}
+          />
+        </Grid>
+      </Grid>
     </React.Fragment>
   );
 }
+
 export default AddCafeForm;
--- frontend/src/container/AddCafe.js
+++ frontend/src/container/AddCafe.js
@@ -14,162 +14,217 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import AddCafeForm from '../component/AddCafeForm';
 import BusinessHourForm from '../component/BusinessHourForm';
 import { useNavigate } from 'react-router-dom';
-import {useState} from 'react';
+import { useState } from 'react';
 import { useParams } from 'react-router-dom';
-import {CREATE_RESTAURANT_MUTATION} from '../graphql/index';
-import { useQuery, useLazyQuery, gql, useMutation } from "@apollo/client";
-import { VariablesAreInputTypesRule } from 'graphql';
-import { message } from "antd";
+import { CREATE_RESTAURANT_MUTATION } from '../graphql/index';
+import { useMutation } from '@apollo/client';
+import { message } from 'antd';
+import '../css/addCafe.css';
 
-const steps = ['Basic Info', 'Business hours'];
+const steps = ['Basic Info', 'Business Hours'];
 
 message.config({
-	top: 80,
+  top: 80,
 });
 
 const theme = createTheme();
 
 function AddCafe() {
-	const navigate = useNavigate();
-	const [activeStep, setActiveStep] = React.useState(0);
-	const { id, name, userid } = useParams();
+  const navigate = useNavigate();
+  const [activeStep, setActiveStep] = React.useState(0);
+  const { id, name, userid } = useParams();
 
-	// Information of new Café
-	const [cafeName, setCafeName] = useState('');
-	const [phoneNum, setPhoneNum] = useState('')
-	const [city, setCity] = useState('');
-	const [district, setDistrict] = useState('');
-	const [address, setAddress] = useState('');
-	const [businessHour, setBusinessHour] = useState([{'name' : 'Monday_Open', 'body' : '07:30'}, {'name' : 'Monday_Close', 'body' : '07:30'},
-													  {'name' : 'Tuesday_Open', 'body' : '07:30'}, {'name' : 'Tuesday_Close', 'body' : '07:30'},
-													  {'name' : 'Wednesday_Open', 'body' : '07:30'}, {'name' : 'Wednesday_Close', 'body' : '07:30'},
-													  {'name' : 'Thursday_Open', 'body' : '07:30'}, {'name' : 'Thursday_Close', 'body' : '07:30'},
-													  {'name' : 'Friday_Open', 'body' : '07:30'}, {'name' : 'Friday_Close', 'body' : '07:30'},
-													  {'name' : 'Saturday_Open', 'body' : '07:30'}, {'name' : 'Saturday_Close', 'body' : '07:30'},
-													  {'name' : 'Sunday_Open', 'body' : '07:30'}, {'name' : 'Sunday_Close', 'body' : '07:30'}])
+  const [cafeName, setCafeName] = useState('');
+  const [phoneNum, setPhoneNum] = useState('');
+  const [city, setCity] = useState('');
+  const [district, setDistrict] = useState('');
+  const [address, setAddress] = useState('');
+  const [businessHour, setBusinessHour] = useState([
+    { name: 'Monday_Open', body: '07:30' },
+    { name: 'Monday_Close', body: '07:30' },
+    { name: 'Tuesday_Open', body: '07:30' },
+    { name: 'Tuesday_Close', body: '07:30' },
+    { name: 'Wednesday_Open', body: '07:30' },
+    { name: 'Wednesday_Close', body: '07:30' },
+    { name: 'Thursday_Open', body: '07:30' },
+    { name: 'Thursday_Close', body: '07:30' },
+    { name: 'Friday_Open', body: '07:30' },
+    { name: 'Friday_Close', body: '07:30' },
+    { name: 'Saturday_Open', body: '07:30' },
+    { name: 'Saturday_Close', body: '07:30' },
+    { name: 'Sunday_Open', body: '07:30' },
+    { name: 'Sunday_Close', body: '07:30' },
+  ]);
 
-	const [createrestaurant] = useMutation(CREATE_RESTAURANT_MUTATION);
+  const [createrestaurant] = useMutation(CREATE_RESTAURANT_MUTATION);
 
-	const displayMessage = (status, msg) => {
-        const content = {
-            content: msg,
-            duration: 1.5,
-        };
-        if(status === 'error') message.error(content);
-        else message.success(content)
+  const displayMessage = (status, msg) => {
+    const content = {
+      content: msg,
+      duration: 1.5,
+    };
+    if (status === 'error') message.error(content);
+    else message.success(content);
+  };
+
+  const handleNext = () => {
+    if (cafeName !== '' && address !== '') {
+      setActiveStep(activeStep + 1);
+    } else {
+      displayMessage('error', 'Please fill out the required fields');
     }
+  };
 
-	const handleNext = () => {
-		if(cafeName !== '' && phoneNum !== '' && city !== '' && district !== '' && address !== ''){
-			setActiveStep(activeStep + 1);
-		}
-		else{
-			displayMessage('error', 'Please fill out the blank');
-		}
-		
-	};
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
 
-	const handleBack = () => {
-		setActiveStep(activeStep - 1);
-	};
+  const getStepContent = (step) => {
+    switch (step) {
+      case 0:
+        return (
+          <AddCafeForm
+            cafeName={cafeName}
+            setCafeName={setCafeName}
+            phoneNum={phoneNum}
+            setPhoneNum={setPhoneNum}
+            city={city}
+            setCity={setCity}
+            district={district}
+            setDistrict={setDistrict}
+            address={address}
+            setAddress={setAddress}
+          />
+        );
+      case 1:
+        return <BusinessHourForm businessHour={businessHour} setBusinessHour={setBusinessHour} />;
+      default:
+        throw new Error('Unknown step');
+    }
+  };
 
-	const getStepContent = (step) => {
-		switch (step) {
-			case 0:
-				return <AddCafeForm cafeName={cafeName} setCafeName={setCafeName} phoneNum={phoneNum} setPhoneNum={setPhoneNum} city={city} setCity={setCity} district={district} setDistrict={setDistrict} address={address} setAddress={setAddress}/>;
-			case 1:
-				return <BusinessHourForm businessHour={businessHour} setBusinessHour={setBusinessHour}/>;
-			default:
-				throw new Error('Unknown step');
-		}
-	}
+  const handleonClick = () => {
+    createrestaurant({
+      variables: {
+        name: cafeName,
+        information: [
+          { name: 'Phone Number', body: phoneNum },
+          { name: 'City', body: city },
+          { name: 'District', body: district },
+          { name: 'Address', body: address },
+          ...businessHour,
+        ],
+      },
+    });
+  };
 
-	const handleonClick = () => {
-		createrestaurant({
-			variables: {
-				name: cafeName,
-				information: [{'name':'Phone Number','body':phoneNum}, {'name':'City','body':city}, {'name':'District','body':district}, {'name':'Address','body':address}, ...businessHour],
-			}
-		})
-	}
-
-	return (
-		<ThemeProvider theme={theme}>
-			<CssBaseline />
-			<AppBar
-				position="absolute"
-				color="default"
-				elevation={0}
-				sx={{
-					position: 'relative',
-					borderBottom: (t) => `1px solid ${t.palette.divider}`,
-				}}
-			>
-				<Toolbar>
-					<Typography variant="h6" color="inherit" noWrap>
-						Add New Café
-					</Typography>
-					<Button style={{position : 'absolute', right:'5%'}}
-						onClick={() => {navigate(`/search/${name}/${userid}`)}}
-					> 
-						Back To Search
-					</Button>
-				</Toolbar>
-				
-			</AppBar>
-			<Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
-				<Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
-					<Typography component="h1" variant="h4" align="center">
-						Café Info
-					</Typography>
-
-					<Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
-						{steps.map((label) => (
-						<Step key={label}>
-							<StepLabel>{label}</StepLabel>
-						</Step>
-						))}
-					</Stepper>
-
-					{activeStep === steps.length ? (
-					<React.Fragment>
-						<Typography variant="h5" gutterBottom>
-							Thank you for your Café Review.
-						</Typography>
-						<Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-								<Button
-									variant="contained"
-									onClick={() => {handleonClick(); navigate('/search/'+name+'/'+userid);}}
-									sx={{ mt: 3, ml: 1 }}
-								>
-									Complete
-								</Button>
-							</Box>
-					</React.Fragment>
-					) : (
-						<React.Fragment>
-							{getStepContent(activeStep)}
-							<Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
-								{activeStep !== 0 && (
-								<Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
-									Back
-								</Button>
-								)}
-
-								<Button
-									variant="contained"
-									onClick={handleNext}
-									sx={{ mt: 3, ml: 1 }}
-								>
-									{activeStep === steps.length - 1 ? 'Done' : 'Next'}
-								</Button>
-							</Box>
-						</React.Fragment>
-					)}
-				</Paper>
-			</Container>
-		</ThemeProvider>
-	);
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <AppBar
+        position="sticky"
+        elevation={3}
+        sx={{
+          background: 'linear-gradient(to right, #FFF8E1, #FFECB3)',
+          color: '#333',
+          boxShadow: '0 2px 6px rgba(0, 0, 0, 0.1)',
+          padding: '10px 20px',
+        }}
+      >
+        <Toolbar sx={{ justifyContent: 'space-between' }}>
+          <Typography
+            variant="h5"
+            component="div"
+            sx={{
+              fontWeight: 'bold',
+              letterSpacing: '1px',
+            }}
+          >
+            Add New Cafe
+          </Typography>
+          <Button
+            variant="contained"
+            onClick={() => navigate(`/search/${name}/${userid}`)}
+            sx={{
+              backgroundColor: '#FB8C00',
+              color: 'white',
+              textTransform: 'none',
+              fontWeight: 'bold',
+              '&:hover': {
+                backgroundColor: '#FF9800',
+              },
+            }}
+          >
+            Back to Search
+          </Button>
+        </Toolbar>
+      </AppBar>
+      <Container
+        component="main"
+        maxWidth="sm"
+        className="container"
+        sx={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          minHeight: '90vh',
+          textAlign: 'center',
+        }}
+      >
+        <Paper
+          variant="outlined"
+          sx={{
+            p: { xs: 2, md: 3 },
+            borderRadius: '8px',
+            background: 'linear-gradient(to bottom, #FFF8E1, #FFF3E0)',
+            width: '100%',
+          }}
+        >
+          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length ? (
+            <React.Fragment>
+              <Typography variant="h5" gutterBottom>
+                Thank you for your Cafe Review.
+              </Typography>
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                <Button
+                  variant="contained"
+                  onClick={() => {
+                    handleonClick();
+                    navigate('/search/' + name + '/' + userid);
+                  }}
+                  sx={{ mt: 3, ml: 1 }}
+                >
+                  Complete
+                </Button>
+              </Box>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              {getStepContent(activeStep)}
+              <Box sx={{ display: 'flex', justifyContent: 'flex-end' }}>
+                {activeStep !== 0 && (
+                  <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                    Back
+                  </Button>
+                )}
+                <Button variant="contained" onClick={handleNext} sx={{ mt: 3, ml: 1 }}>
+                  {activeStep === steps.length - 1 ? 'Done' : 'Next'}
+                </Button>
+              </Box>
+            </React.Fragment>
+          )}
+        </Paper>
+      </Container>
+    </ThemeProvider>
+  );
 }
 
 export default AddCafe;
